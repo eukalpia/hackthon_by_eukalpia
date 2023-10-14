3c96ade9663e5b6d68440a061c40ee700ed3f0bd
eukalpia <120781817+eukalpia@users.noreply.github.com>
2023-10-14 19:29:05 +0500

    model.py

--- Chatbot/model.py
+++ Chatbot/model.py
@@ -1,20 +1,16 @@
-import torch
-import torch.nn as nn
-
-
-class NeuralNet(nn.Module):
+import torch  # Импортируем библиотеку torch
+import torch.nn as nn  # Импортируем модуль nn из torch
+class NeuralNet(nn.Module):  # Определяем класс NeuralNet, который расширяет nn.Module
     def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.l2 = nn.Linear(hidden_size, hidden_size) 
-        self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
-    
+        super(NeuralNet, self).__init__()  # Вызываем конструктор родительского класса
+        self.l1 = nn.Linear(input_size, hidden_size)  # Создаем линейный слой
+        self.l2 = nn.Linear(hidden_size, hidden_size)  # Создаем линейный слой
+        self.l3 = nn.Linear(hidden_size, num_classes)  # Создаем линейный слой
+        self.relu = nn.ReLU()  # Создаем функцию активации ReLU
     def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        out = self.relu(out)
-        out = self.l3(out)
-        # no activation and no softmax at the end
+        out = self.l1(x)  # Проход через первый линейный слой
+        out = self.relu(out)  # Применение функции активации ReLU
+        out = self.l2(out)  # Проход через второй линейный слой
+        out = self.relu(out)  # Применение функции активации ReLU
+        out = self.l3(out)  # Проход через третий линейный слой
         return out
